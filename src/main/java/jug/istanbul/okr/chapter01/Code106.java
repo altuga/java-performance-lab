773b407c7b42b305f350c744999bb0b39d871481
Altug B. Altintas <altuga@gmail.com>
2023-01-04 17:41:49 +0300

    Chapter 2 go on

---
+++ src/main/java/jug/istanbul/okr/chapter01/Code106.java
@@ -0,0 +1,15 @@
+package jug.istanbul.okr.chapter01;
+
+public class Code106 {
+
+    public void find(boolean check) {
+        int answer;int otherAnswer;
+        if (check) {
+            otherAnswer = 1;
+            answer = 1;
+        } else {
+            answer = 2;
+        }
+        //System.out.print(otherAnswer + answer);
+    }
+}
