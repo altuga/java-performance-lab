76c899ad3a7952e16156950a64e5fbc6e18120a1
Altug B. Altintas <altuga@gmail.com>
2023-03-01 14:32:26 +0300

    new questions added

--- src/main/java/jug/istanbul/okr/chapter07/Snake.java
+++ src/main/java/jug/istanbul/okr/chapter07/Snake.java
@@ -1,5 +1,29 @@
 package jug.istanbul.okr.chapter07;
 
-// Snake.java
-public sealed class Snake permits Cobra {}
-final class Cobra extends Snake {}
+
+public sealed class Snake permits Cobra {
+    protected boolean getTroutName() {
+        return true;
+    }
+}
+
+final class Cobra extends Snake {
+
+    public void printName(Snake snake) {
+        System.out.println(switch (snake) {
+            case Cobra c -> c.getBassName();
+            case Snake s -> s.getTroutName();
+        });
+    }
+
+    private boolean getBassName() {
+        return false;
+    }
+}
+
+final class TestSnake {
+    public static void main(String[] args) {
+        Cobra cobra = new Cobra();
+        cobra.printName(cobra);
+    }
+}
