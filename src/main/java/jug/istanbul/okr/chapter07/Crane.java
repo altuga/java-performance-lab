152c96b2b6a4fda00b1d1a2762a7fd13fa03cfaf
Altug B. Altintas <altuga@gmail.com>
2023-03-03 16:33:02 +0300

    Chapter 7 added

--- src/main/java/jug/istanbul/okr/chapter07/Crane.java
+++ src/main/java/jug/istanbul/okr/chapter07/Crane.java
@@ -1,7 +1,11 @@
 package jug.istanbul.okr.chapter07;
 
 public record Crane(int numberEggs, String name) {
-
+    public Crane {
+        if (numberEggs < 0)
+            throw new IllegalArgumentException();
+     
+    }
 }
 
 class TestCrane {
