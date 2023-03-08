bd624170ec02d41f736326bf58a8165f73ea580a
Altug B. Altintas <altuga@gmail.com>
2023-03-08 15:12:37 +0300

    code added

---
+++ src/main/java/jug/istanbul/okr/chapter03/Jaws.java
@@ -0,0 +1,20 @@
+package jug.istanbul.okr.chapter03;
+
+public class Jaws {
+
+    public static void main(String[] args) {
+        int penguin = 50, turtle = 75;
+        boolean older = penguin >= turtle;
+        if (older = true) System.out.println("Success");
+        else System.out.println("Failure");
+    }
+
+    
+
+    /**
+     * F. Line 19 starts with an else statement, but there is no preceding if statement that it
+     * matches. For this reason, line 19 does not compile, making option F the correct answer. If the
+     * else keyword was removed from line 19, then the code snippet would print Success.
+     */
+
+}
