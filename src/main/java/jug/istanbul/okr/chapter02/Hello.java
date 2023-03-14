18ca582c05cc071fc60d58709037ab78836c8c82
Altug B. Altintas <altuga@gmail.com>
2023-03-14 14:18:03 +0300

    expriments

--- src/main/java/jug/istanbul/okr/chapter02/Hello.java
+++ src/main/java/jug/istanbul/okr/chapter02/Hello.java
@@ -10,6 +10,48 @@ public class Hello {
         System.out.print(elma + " | " + armut);
     }
 
+    // write a method that counts  ends with  "r" and "s"  for the input
+    // use regex
+
+
+    public static int countRS2(String input) {
+        int count = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == 'r' || input.charAt(i) == 's') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int countRS3(String input) {
+        int count = 0;
+        for (char c : input.toCharArray()) {
+            if (c == 'r' || c == 's') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int countRS4(String input) {
+        return (int) input.chars()
+                .filter(c -> c == 'r' || c == 's')
+                .count();
+    }t
+   
+
+
+
+
+
+     
+
+   
+
+
+
+
     public static void main(String[] args) {
         run();
     }
--- src/main/java/jug/istanbul/okr/chapter02/Store.java
+++ src/main/java/jug/istanbul/okr/chapter02/Store.java
@@ -4,6 +4,8 @@ public class Store  {
     static double add(double fruit, float vegetables) {
         return fruit + vegetables;
     }
+ 
+    // write a simple sum method hello
 
     public static void main(String[] args) {
         System.out.print(add(1.4, 2.4f) + ", ");
