7faac7f8a93e6f672cb567cc13ed3eef2620659f
Altug B. Altintas <altuga@gmail.com>
2023-03-21 22:57:02 +0300

    new questions added

--- src/main/java/jug/istanbul/okr/chapter01/Mouse.java
+++ src/main/java/jug/istanbul/okr/chapter01/Mouse.java
@@ -1,17 +1,47 @@
 package jug.istanbul.okr.chapter01;
 
-public class Mouse {
- final static int MAX_LENGTH = 5;
- int length;
- public void grow(int inches) {
-   if (length < MAX_LENGTH) {
-     int newSize = length + inches;
-     length = newSize;
-    }
-  }
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
-  public static void main(String args[]) {
-    Mouse mouse = new Mouse();
-    mouse.grow(10);
-  }
+public class Mouse {
+    final static int MAX_LENGTH = 5;
+    int length;
+
+    public void grow(int inches) {
+        if (length < MAX_LENGTH) {
+            int newSize = length + inches;
+            length = newSize;
+        }
+    }
+
+    public static void main(String args[]) throws IOException {
+        Mouse mouse = new Mouse();
+        mouse.grow(10);
+
+
+        //region Description
+        File file = new File("delete.txt");
+
+
+        List<String> strings = new ArrayList<>();
+        String line;
+        //endregion
+
+
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file));){
+            strings = reader.lines().filter(l-> !l.isBlank()).filter(l -> l.length() > 3).toList();
+        }
+
+
+
+        String hello = "hello";
+
+        for (String string : strings) {
+
+        }
+
+
+    }
 }
